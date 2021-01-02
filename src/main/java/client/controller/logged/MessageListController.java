a745de7e4634197d3e5e7f4b67eb868156ac757e
Gianluca Cognigni <cog.gianluca@gmail.com>
2021-01-02 16:03:59 +0100

    need commit to switch branch

--- src/main/java/client/controller/logged/MessageListController.java
+++ src/main/java/client/controller/logged/MessageListController.java
@@ -14,12 +14,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MessageListController implements Initializable {
 
 
-    private ObservableList<Mail> mailObservableList = FXCollections.observableArrayList();
+    private ObservableList<Mail> inboxList = FXCollections.observableArrayList();
+    private ObservableList<Mail> outboxList = FXCollections.observableArrayList();
 
     @FXML
     private ListView mailList;
@@ -28,36 +30,78 @@ public class MessageListController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadMessages();
-        mailList.setItems(mailObservableList);
+        loadInbox(); //come carico la inbox ora che devo passare l'attributo utente ?
+        mailList.setItems(inboxList);
         mailList.setCellFactory((Callback<ListView<Mail>, ListCell<Mail>>) listView -> new MailCell());
 
     }
 
-    private void loadMessages() {
+    private void loadInbox(String user) {
         // Generate random values to fill listview.
         //TODO: Get data from server
+        ArrayList<Mail> cachedInbox = new ArrayList<>();
+        ObjectInputStream receiveInbox = null;
+        ObjectOutputStream sendRequests = null;
         try {
-            Socket s = new Socket(host,8189);
+            Socket s = new Socket(host, 8189);
             System.out.println("Connection established");
-            ObjectInputStream receiveBoxes= new ObjectInputStream(s.getInputStream());
-            ObjectOutputStream sendRequests = new ObjectOutputStream(s.getOutputStream());
-            sendRequests.write();
-        }catch(IOException e) {
+            receiveInbox = new ObjectInputStream(s.getInputStream());
+            sendRequests = new ObjectOutputStream(s.getOutputStream());
+            sendRequests.writeUTF(user);
+            sendRequests.writeUTF("Inbox");
+            cachedInbox = (ArrayList<Mail>) receiveInbox.readObject();
+            for (Mail mail : cachedInbox) {
+                inboxList.add(mail);
+            }
+        } catch (IOException | ClassNotFoundException e) { //serve più granulare per l'aggiornamento inbox costante ? granulare = singole mail
+            System.out.println("Receiving inbox failed");
             System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (receiveInbox != null)
+                    receiveInbox.close();
+                if (sendRequests != null)
+                    sendRequests.close();
+            } catch (IOException ex) {
+                System.out.println("closing connection failed");
+                System.out.println(ex.getMessage());
+            }
         }
 
 
-
-
-        for(int i = 0; i < 10; i++) {
-            Mail m = new Mail();
-            m.setFrom("Da utente "); m.setObject("oggetto");
-            m.setTo("A utente" + i ); m.setMessage("TestoTestoTestoTestoTestoTestoTestoTesto " + i );
-            mailObservableList.add(m);
-        }
-        System.out.println(" ci son " + mailObservableList.size());
     }
+    private void loadOutbox(String user) {
+        // Generate random values to fill listview.
+        //TODO: Get data from server
+        ArrayList<Mail> cachedInbox = new ArrayList<>();
+        ObjectInputStream receiveOutbox = null;
+        ObjectOutputStream sendRequests = null;
+        try {
+            Socket s = new Socket(host, 8189);
+            System.out.println("Connection established");
+            receiveOutbox = new ObjectInputStream(s.getInputStream());
+            sendRequests = new ObjectOutputStream(s.getOutputStream());
+            sendRequests.writeUTF(user);
+            sendRequests.writeUTF("Outbox");
+            cachedInbox = (ArrayList<Mail>) receiveOutbox.readObject();
+            for (Mail mail : cachedInbox) {
+                inboxList.add(mail);
+            }
+        } catch (IOException | ClassNotFoundException e) { //serve più granulare per l'aggiornamento inbox costante ? granulare = singole mail
+            System.out.println("Receiving outbox failed");
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (receiveOutbox != null)
+                    receiveOutbox.close();
+                if (sendRequests != null)
+                    sendRequests.close();
+            } catch (IOException ex) {
+                System.out.println("closing connection failed");
+                System.out.println(ex.getMessage());
+            }
+        }
 
 
+    }
 }
