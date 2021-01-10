a0280ee03a0763236bda63437f899fb75f57de61
syscall00 <73221567+syscall00@users.noreply.github.com>
2021-01-10 18:53:41 +0100

    merged branch

--- src/main/java/models/Mail.java
+++ src/main/java/models/Mail.java
@@ -1,75 +1,86 @@
 package models;
 
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // Model
 public class Mail implements Serializable {
 
-    private long id;
+    private transient LongProperty id;
 
-    private User sender;
-    private List<User> receiver;
+    private transient User sender;
+    private transient ListProperty<User> receiver;
 
-    private String object;
-    private String message;
-    private boolean sent;
+    private transient StringProperty object;
+    private transient StringProperty message;
+    private transient BooleanProperty sent;
 
     public boolean isSent() {
-        return sent;
+        return sent.get();
     }
 
     public void setSent(boolean sent) {
-        this.sent = sent;
+        this.sent.set(sent);
     }
 
     public Mail(long id, User sender, List<User> receiver, String object, String message) {
-        this.id = id;
+        this.id = new SimpleLongProperty(id);
         this.sender = sender;
-        this.receiver = receiver;
-        this.object = object;
-        this.message = message;
+        this.receiver = new SimpleListProperty<User>();
+        if (receiver != null) setReceiver(receiver);
+        this.object = new SimpleStringProperty(object);
+        this.message = new SimpleStringProperty(message);
     }
 
     public long getId() {
-        return id;
+        return id.get();
     }
 
     public void setId(long id) {
-        this.id = id;
+        this.id.set(id);
     }
+
+
 
     public User getSender() {
         return sender;
     }
-
     public void setSender(User sender) {
         this.sender = sender;
     }
 
+
+
     public List<User> getReceiver() {
-        return receiver;
+        return receiver.get();
     }
 
     public void setReceiver(List<User> receiver) {
-        this.receiver = receiver;
+        this.receiver.clear();
+        this.receiver.addAll(receiver);
     }
 
     public String getObject() {
-        return object;
+        return object.get();
     }
 
     public void setObject(String object) {
-        this.object = object;
+        this.object.set(object);
     }
 
     public String getMessage() {
-        return message;
+        return message.get();
     }
 
     public void setMessage(String message) {
-        this.message = message;
+        this.message.set(message);
     }
 
     @Override
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,19 +1,42 @@
 package models;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    private String username;
+    private transient StringProperty username;
 
     public User(String username) {
-        this.username = username;
+        this.username = new SimpleStringProperty(username);
     }
 
+
     public String getUsername() {
-        return username;
+        return username.get();
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.username.set(username);
     }
+
+    private void init() {
+        this.username = new SimpleStringProperty();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+
+        out.writeUTF(getUsername());
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        init();
+        setUsername(in.readUTF());
+    }
+
+
 }
